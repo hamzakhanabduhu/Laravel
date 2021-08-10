90e7b22f03bf8b202be2a5be452dcd077aea9d4c
Muhammad Hamza Khan <hamza.khan2578@gmail.com>
2021-08-10 16:01:33 +0500

    laravel

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -41,6 +41,7 @@
         </div>
     </div>
 </div>
+
 <div id="my-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="my-modal-title" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
